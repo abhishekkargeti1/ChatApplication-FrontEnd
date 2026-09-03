501d022c10d255fcdff062ebf739f9d6cd9abd16
Abhishek kargeti <abhishek.kargeti1@gmail.com>
2026-09-03 21:13:36 +0530

    Update AxiosHelper.js

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -8,26 +8,3 @@ export const httpClient = axios.create({
 });
 
 
-// import axios from "axios";
-
-// // Dynamically use the current host and protocol without hardcoding port 8080
-// const getBaseURL = () => {
-//   if (import.meta.env.VITE_API_URL) {
-//     return import.meta.env.VITE_API_URL;
-//   }
-  
-//   const host = window.location.hostname;
-//   const protocol = window.location.protocol;
-  
-//   // Connects via standard port 80/443 directly to Ingress
-//   return `${protocol}//${host}`;
-// };
-
-// export const baseURL = getBaseURL();
-
-// export const httpClient = axios.create({
-//   baseURL: `${baseURL}/api`,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });

dd869a9657ec9f179ab3def1677d95b758410753
Abhishek kargeti <abhishek.kargeti1@gmail.com>
2026-09-05 12:30:44 +0530

    deployment updated

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,15 +1,7 @@
-// import axios from "axios";
-
-// // Corrected URL (removed the trailing slash before :8080)
-// export const baseURL = "http://3.92.146.31:8080"; 
-
-// export const httpClient = axios.create({
-//   baseURL: `${baseURL}/api`,
-// });
-
 import axios from "axios";
 
-export const baseURL = "http://localhost:8083"; 
+// Corrected URL (removed the trailing slash before :8080)
+export const baseURL = "http://3.92.146.31:8080"; 
 
 export const httpClient = axios.create({
   baseURL: `${baseURL}/api`,

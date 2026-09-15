a1e0dc83df56616c9d4790e03a56868b83db5f95
Abhishek kargeti <abhishek.kargeti1@gmail.com>
2026-09-15 10:09:18 +0530

    files update

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,11 +1,10 @@
- import axios from "axios";
+import axios from "axios";
 
-// Dynamically points to your Ingress URL host
-//export const baseURL = "http://98.92.245.161:8080"; 
-export const baseURL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
-console.log(baseURL)
+// Dynamically points to the current host
+export const baseURL = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ""}`;
+
+console.log("Base URL:", baseURL);
+
 export const httpClient = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: `${baseURL}/chatapi`,
 });
-
-
